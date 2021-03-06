d9e632f4198da6c46bd0e34568824c7587364732
ASUS <alitavakkoli80@gmail.com>
2021-03-06 22:01:07 +0330

    [MOD] comenting and adding javadoc to project

--- src/ir/ac/kntu/Voronoi.java
+++ src/ir/ac/kntu/Voronoi.java
@@ -18,12 +18,14 @@ public class Voronoi {
     private int size;
     private Color[] randColor;
 
+    /**
+     * @param sceneSize size of the window
+     * @param cells number of random points
+     */
     public Voronoi(int sceneSize, int cells) {
-        // root node of scene graph
         this.cells = cells;
         this.size = sceneSize;
         Pane pane = new Pane();
-        // create and add a circle to the pane
         int n = 0;
         Random rand = new Random();
         this.px = new int[cells];
@@ -34,37 +36,38 @@ public class Voronoi {
             this.py[i] = rand.nextInt(size);
             this.randColor[i] = Color.rgb(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
         }
-
+        //Coloring the scene by placing circles on each pixels
         for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
                 n = 0;
                 for (byte i = 0; i < cells; i++) {
                     if (distance(px[i], x, py[i], y) < distance(px[n], x, py[n], y)) {
                         n = i;
-
                     }
                 }
-//                Rectangle rectangle = new Rectangle(x,y,1,1);
-//                rectangle.setFill(randColor[n]);
                 Circle circle = new Circle(x, y, 1, randColor[n]);
                 pane.getChildren().add(circle);
-//                pane.getChildren().add(rectangle);
-
             }
         }
-
+        //Placing random points on pane
         for (int i = 0; i < cells; i++) {
             Circle circle1 = new Circle(px[i], py[i], 3, BLACK);
             pane.getChildren().add(circle1);
         }
 
-//         scene to be shown on stage
         this.voronoiScene = new Scene(pane, size, size, LIGHTSTEELBLUE);
     }
 
+    /**
+     * @param x1 x of the first point
+     * @param x2 x of the second point
+     * @param y1 y of the first point
+     * @param y2 y of the second point
+     * @return distance between two points
+     */
     private double distance(int x1, int x2, int y1, int y2) {
         double d;
-        d = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)); // Euclidian
+        d = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
         return d;
     }
 
