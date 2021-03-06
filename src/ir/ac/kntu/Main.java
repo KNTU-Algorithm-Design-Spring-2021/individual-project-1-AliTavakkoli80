83c8c55cd37fa0be5d6320546bbeff201e2d1600
ASUS <alitavakkoli80@gmail.com>
2021-03-06 17:32:26 +0330

    [ADD] some for and loops Description : Project completed ,I am going to write it cleaner

--- src/ir/ac/kntu/Main.java
+++ src/ir/ac/kntu/Main.java
@@ -1,6 +1,7 @@
 package ir.ac.kntu;
 
 import javafx.application.Application;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -8,7 +9,9 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
-import static javafx.scene.paint.Color.LIGHTSTEELBLUE;
+import java.util.Random;
+
+import static javafx.scene.paint.Color.*;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -21,14 +24,42 @@ public class Main extends Application {
         Pane pane = new Pane();
 //        Rectangle rectangle = ne
         // create and add a circle to the pane
-        Circle circle1 = new Circle(300, 300, 300, Color.GREEN);
-        Circle circle2 = new Circle(300,300,200,Color.WHITE);
-        Circle circle3 = new Circle(300,300,100,Color.RED);
-        pane.getChildren().add(circle1);
-        pane.getChildren().add(circle2);
-        pane.getChildren().add(circle3);
+        int px[], py[], cells = 50, size = 600;
+        int n = 0;
+        Color randColor[];
+        Random rand = new Random();
+        px = new int[cells];
+        py = new int[cells];
+        randColor = new Color[cells];
+        for (int i = 0; i < cells; i++) {
+            px[i] = rand.nextInt(size);
+            py[i] = rand.nextInt(size);
+            randColor[i] = Color.rgb(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
+        }
+
+        for (int x = 0; x < size; x++) {
+
+            for (int y = 0; y < size; y++) {
+                n = 0;
+                for (byte i = 0; i < cells; i++) {
+                    if (distance(px[i], x, py[i], y) < distance(px[n], x, py[n], y)) {
+                        n = i;
+
+                    }
+                }
+                Circle circle1 = new Circle(x, y, 1, randColor[n]);;
+                pane.getChildren().add(circle1);
+
+            }
+        }
+
+        for (int i = 0; i < cells; i++) {
+            Circle circle1 = new Circle(px[i], py[i], 3, BLACK);;
+            pane.getChildren().add(circle1);
+        }
+
 //         scene to be shown on stage
-        Scene scene = new Scene(pane,600,600, LIGHTSTEELBLUE);
+        Scene scene = new Scene(pane,size,size, LIGHTSTEELBLUE);
 
         // set a cool name for stage
         stage.setTitle("Javafx Starter");
@@ -38,5 +69,12 @@ public class Main extends Application {
 
         // and now ... put up a SHOW
         stage.show();
+    }
+    static double distance(int x1, int x2, int y1, int y2) {
+        double d;
+        d = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)); // Euclidian
+//          d = Math.abs(x1 - x2) + Math.abs(y1 - y2); // Manhattan
+//          d = Math.pow(Math.pow(Math.abs(x1 - x2), p) + Math.pow(Math.abs(y1 - y2), p), (1 / p)); // Minkovski
+        return d;
     }
 }
--- src/ir/ac/kntu/Voronoi.java
+++ src/ir/ac/kntu/Voronoi.java
@@ -1,4 +1,18 @@
 package ir.ac.kntu;
 
+import javafx.geometry.Point2D;
+import java.util.Random;
+
 public class Voronoi {
+    private static int cells = 100, size = 1000;
+    Point2D randomPoints[];
+
+    public Voronoi() {
+        randomPoints = new Point2D[cells];
+        Random rand = new Random();
+        for (int i = 0; i < cells; i++) {
+            this.randomPoints[i].add((double)rand.nextInt(size),(double)rand.nextInt(size));
+        }
+    }
+
 }
