f244b73e237b0e3222094a42d80eaa7a42bddb88
ASUS <alitavakkoli80@gmail.com>
2021-03-06 22:10:24 +0330

    [ADD] adding some photos

--- src/ir/ac/kntu/Main.java
+++ src/ir/ac/kntu/Main.java
@@ -4,8 +4,8 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    private static int Size = 800;
-    private static int numCells = 100;
+    private static final int Size = 800;
+    private static final int numCells = 100;
 
     public static void main(String[] args) {
         launch(args);
@@ -15,13 +15,8 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
 
         Voronoi voronoi = new Voronoi(Size,numCells);
-        // set a cool name for stage
         stage.setTitle(" Voronoi Diagram ");
-
-        // now give the scene to the stage for show
         stage.setScene(voronoi.getVoronoiScene());
-
-        // and now ... put up a SHOW
         stage.show();
     }
 }
