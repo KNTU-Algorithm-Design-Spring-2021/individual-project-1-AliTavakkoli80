ceda0a2da824900b5e26ea70e3f2e5bae4b796fe
ASUS <alitavakkoli80@gmail.com>
2021-03-06 21:46:56 +0330

    [MOD] Voronoi class completed and Main class is cleaner

--- src/ir/ac/kntu/Main.java
+++ src/ir/ac/kntu/Main.java
@@ -1,80 +1,27 @@
 package ir.ac.kntu;
 
 import javafx.application.Application;
-import javafx.geometry.Point2D;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
-import java.util.Random;
-
-import static javafx.scene.paint.Color.*;
-
 public class Main extends Application {
+    private static int Size = 800;
+    private static int numCells = 100;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        // root node of scene graph
-        Pane pane = new Pane();
-//        Rectangle rectangle = ne
-        // create and add a circle to the pane
-        int px[], py[], cells = 50, size = 600;
-        int n = 0;
-        Color randColor[];
-        Random rand = new Random();
-        px = new int[cells];
-        py = new int[cells];
-        randColor = new Color[cells];
-        for (int i = 0; i < cells; i++) {
-            px[i] = rand.nextInt(size);
-            py[i] = rand.nextInt(size);
-            randColor[i] = Color.rgb(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
-        }
 
-        for (int x = 0; x < size; x++) {
-
-            for (int y = 0; y < size; y++) {
-                n = 0;
-                for (byte i = 0; i < cells; i++) {
-                    if (distance(px[i], x, py[i], y) < distance(px[n], x, py[n], y)) {
-                        n = i;
-
-                    }
-                }
-                Circle circle1 = new Circle(x, y, 1, randColor[n]);;
-                pane.getChildren().add(circle1);
-
-            }
-        }
-
-        for (int i = 0; i < cells; i++) {
-            Circle circle1 = new Circle(px[i], py[i], 3, BLACK);;
-            pane.getChildren().add(circle1);
-        }
-
-//         scene to be shown on stage
-        Scene scene = new Scene(pane,size,size, LIGHTSTEELBLUE);
-
+        Voronoi voronoi = new Voronoi(Size,numCells);
         // set a cool name for stage
-        stage.setTitle("Javafx Starter");
+        stage.setTitle(" Voronoi Diagram ");
 
         // now give the scene to the stage for show
-        stage.setScene(scene);
+        stage.setScene(voronoi.getVoronoiScene());
 
         // and now ... put up a SHOW
         stage.show();
-    }
-    static double distance(int x1, int x2, int y1, int y2) {
-        double d;
-        d = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)); // Euclidian
-//          d = Math.abs(x1 - x2) + Math.abs(y1 - y2); // Manhattan
-//          d = Math.pow(Math.pow(Math.abs(x1 - x2), p) + Math.pow(Math.abs(y1 - y2), p), (1 / p)); // Minkovski
-        return d;
     }
 }
--- src/ir/ac/kntu/Voronoi.java
+++ src/ir/ac/kntu/Voronoi.java
@@ -1,18 +1,74 @@
 package ir.ac.kntu;
 
-import javafx.geometry.Point2D;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 import java.util.Random;
 
-public class Voronoi {
-    private static int cells = 100, size = 1000;
-    Point2D randomPoints[];
+import static javafx.scene.paint.Color.BLACK;
+import static javafx.scene.paint.Color.LIGHTSTEELBLUE;
 
-    public Voronoi() {
-        randomPoints = new Point2D[cells];
+public class Voronoi {
+    private Scene voronoiScene;
+    private int[] px;
+    private int[] py;
+    private int cells;
+    private int size;
+    private Color[] randColor;
+
+    public Voronoi(int sceneSize, int cells) {
+        // root node of scene graph
+        this.cells = cells;
+        this.size = sceneSize;
+        Pane pane = new Pane();
+        // create and add a circle to the pane
+        int n = 0;
         Random rand = new Random();
+        this.px = new int[cells];
+        this.py = new int[cells];
+        this.randColor = new Color[cells];
         for (int i = 0; i < cells; i++) {
-            this.randomPoints[i].add((double)rand.nextInt(size),(double)rand.nextInt(size));
+            this.px[i] = rand.nextInt(size);
+            this.py[i] = rand.nextInt(size);
+            this.randColor[i] = Color.rgb(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
         }
+
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                n = 0;
+                for (byte i = 0; i < cells; i++) {
+                    if (distance(px[i], x, py[i], y) < distance(px[n], x, py[n], y)) {
+                        n = i;
+
+                    }
+                }
+//                Rectangle rectangle = new Rectangle(x,y,1,1);
+//                rectangle.setFill(randColor[n]);
+                Circle circle = new Circle(x, y, 1, randColor[n]);
+                pane.getChildren().add(circle);
+//                pane.getChildren().add(rectangle);
+
+            }
+        }
+
+        for (int i = 0; i < cells; i++) {
+            Circle circle1 = new Circle(px[i], py[i], 3, BLACK);
+            pane.getChildren().add(circle1);
+        }
+
+//         scene to be shown on stage
+        this.voronoiScene = new Scene(pane, size, size, LIGHTSTEELBLUE);
     }
 
+    private double distance(int x1, int x2, int y1, int y2) {
+        double d;
+        d = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)); // Euclidian
+        return d;
+    }
+
+    public Scene getVoronoiScene() {
+        return voronoiScene;
+    }
 }
